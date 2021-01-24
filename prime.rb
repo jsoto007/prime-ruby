e0168fd7acc158a72877dd9e20579f8a7b364849
Jose Soto <jsoto7087@gmail.com>
2021-01-24 18:06:53 -0500

    commit tree 77cf1370558d05545e77ed77cebf990fb8ad8fe4

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+
+#define a method prime?(), that takes in an integer argument and returns a boolean 
+    # -- or whether or not the integer is a prime number
+        #create an algoritm that determines if a number is aprime
+            # create a range of numbers in an array
+
+# # Add  code here!
+
+def prime?(x)
+    if x == 1
+      return false 
+    elsif x < 1
+      return false
+    else
+    for y in 2..(x-1)
+      if (x % y) == 0
+        return false 
+      end
+    end 
+  end
+  
+    true
+  end
